40ebf6e0783ccec33ad9b024859f6a770e8f6e75
Peter Seibel <peter@gigamonkeys.com>
2022-07-08 21:07:09 -0700

    Add code to create checkpoints branch.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-import { request } from "@octokit/request";
-import { Octokit } from "@octokit/rest";
 import netlify from "netlify-auth-providers";
+import { Octokit } from "@octokit/rest";
 
-// Simulated file content.
-const fileContent = "{foo: 'bar'}";
+// TODO:
+//
+// stash access token in cookie? And check if it still works before authing again?
+//
 
 const scopes = ["repo", "user"];
 const site_id = "1d7e043c-5d02-47fa-8ba8-9df0662ba82b";
@@ -27,6 +28,9 @@ const u = await octokit.rest.users.getAuthenticated().catch((e) => false);
 
 const toJSON = (r) => JSON.stringify(r, null, 2);
 
+// Simulated file content.
+const fileContent = toJSON({ foo: "bar", baz: "quux" });
+
 let out = "";
 
 if (u) {
@@ -47,7 +51,7 @@ if (u) {
   }
 
   const file = await octokit
-    .request("GET /repos/{owner}/{repo}/contents/{path}", { ...repo, path: "config.json" })
+    .request("GET /repos/{owner}/{repo}/contents/{path}", { ...repo, path: "config.json", ref: "checkpoints" })
     .catch(false);
 
   if (file) {
@@ -63,6 +67,7 @@ if (u) {
           message: "Making config file",
           content: btoa(fileContent),
           sha: file.data.sha,
+          branch: "checkpoints",
         })
         .then(toJSON);
     }
@@ -76,6 +81,33 @@ if (u) {
         content: btoa(fileContent),
       })
       .then(toJSON);
+  }
+
+  const main = await octokit
+    .request("GET /repos/{owner}/{repo}/git/ref/{ref}", { ...repo, ref: "heads/main" })
+    .catch((e) => false);
+
+  const checkpoints = await octokit
+    .request("GET /repos/{owner}/{repo}/git/ref/{ref}", { ...repo, ref: "heads/checkpoints" })
+    .catch((e) => false);
+
+  if (main) {
+    out += `\nmain: ${toJSON(main)}\n`;
+
+    if (!checkpoints) {
+      out += await octokit
+        .request("POST /repos/{owner}/{repo}/git/refs", {
+          ...repo,
+          ref: "refs/heads/checkpoints",
+          sha: main.data.object.sha,
+        })
+        .then((checkpoints) => `\ncheckpoints:${toJSON(checkpoints)}\n`)
+        .catch((error) => `Couldn't make checkpoints: ${error}`);
+    } else {
+      out += `\ncheckpoints already exists: ${toJSON(checkpoints)}\n`;
+    }
+  } else {
+    out += "Couldn't find main.";
   }
 } else {
   out += "Couldn't get user!";
