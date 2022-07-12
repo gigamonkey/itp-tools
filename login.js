35715434bcef6ae35867e2f91e663ac2a010666e
Peter Seibel <peter@gigamonkeys.com>
2022-07-11 20:37:02 -0700

    Moving functions around.

--- login.js
+++ login.js
@@ -3,38 +3,6 @@ import github from './modules/github';
 const REPO_NAME = 'itp';
 const SITE_ID = '1d7e043c-5d02-47fa-8ba8-9df0662ba82b';
 
-// FIXME: this is where we could provide an override mechanism in case someone
-// already has an 'itp' repo. And because this is already async we'll be able to
-// make another API call or something.
-const getRepoName = async (login) => {
-  console.log(`Getting repo name for ${login}`);
-  return REPO_NAME;
-};
-
-const makeRepo = async (gh, name) => {
-  // Redirect to assignments page on success, otherwise show error message.
-  try {
-    gh.makeRepo(name);
-    window.location = '/';
-  } catch (e) {
-    show('problem-making-repo', name);
-  }
-};
-
-// Make this async so later we can do something that actually requires
-// talking to GitHub to check out the repo.
-const wellFormed = async (repo) => Promise.resolve(true);
-
-/*
- * Show a hidden div, filling in any .repo-name elements first.
- */
-const show = (id, repoName) => {
-  const div = document.getElementById(id);
-  div.querySelectorAll('.repo-name').forEach((e) => {
-    e.innerText = repoName;
-  });
-  div.hidden = false;
-};
 
 const connectToRepo = async () => {
   // Login to Github.
@@ -64,5 +32,48 @@ const connectToRepo = async () => {
     };
   }
 };
+
+/*
+ * This is where we could provide an override mechanism in case someone already
+ * has an 'itp' repo. And because this is already async we'll be able to make
+ * another API call or something.
+*/
+const getRepoName = async (login) => {
+  console.log(`Getting repo name for ${login}`);
+  return REPO_NAME;
+};
+
+/*
+ * Create the repo we will use to save files in. Probably should actually be
+ * made from a template.
+ */
+const makeRepo = async (gh, name) => {
+  // Redirect to assignments page on success, otherwise show error message.
+  try {
+    gh.makeRepo(name);
+    window.location = '/';
+  } catch (e) {
+    show('problem-making-repo', name);
+  }
+};
+
+
+/*
+ * Make this async so later we can do something that actually requires talking
+ * to GitHub to check out the repo.
+ */
+const wellFormed = async (repo) => Promise.resolve(true);
+
+/*
+ * Show a hidden div, filling in any .repo-name elements first.
+ */
+const show = (id, repoName) => {
+  const div = document.getElementById(id);
+  div.querySelectorAll('.repo-name').forEach((e) => {
+    e.innerText = repoName;
+  });
+  div.hidden = false;
+};
+
 
 connectToRepo();
