1ce3d7e8fd1581989f32b8505eb8dbe9f1240e0f
Peter Seibel <peter@gigamonkeys.com>
2022-07-18 18:17:58 -0700

    Make anonymity sticky (with 15 minute TTL) within a session.

--- modules/login.js
+++ modules/login.js
@@ -1,9 +1,11 @@
+const KEY = 'anonymousUntil';
+const TTL = 15 * 60 * 1000; // fifteen minutes in milliseconds.
+
 class Login {
   constructor() {
     this.username = null;
     this.profileURL = null;
     this.isMember = false;
-    this.anonymous = false;
     this.problemMakingRepo = null;
     this.createdRepo = false;
     this.repoURL = null;
@@ -13,6 +15,31 @@ class Login {
     this.username = username;
     this.profileURL = profileURL;
   }
+
+  get anonymous() {
+    const v = sessionStorage.getItem(KEY);
+    if (v === null) {
+      return false;
+    } else {
+      const expiresAt = Number.parseInt(v, 10);
+      if (expiresAt < Date.now()) {
+        sessionStorage.removeItem(KEY);
+        return false;
+      } else {
+        sessionStorage.setItem(KEY, String(Date.now() + TTL));
+        return true;
+      }
+    }
+  }
+
+  set anonymous(value) {
+    if (value) {
+      sessionStorage.setItem(KEY, String(Date.now() + TTL));
+    } else {
+      sessionStorage.removeItem(KEY);
+    }
+  }
+
 
   get isLoggedIn() {
     return this.username !== null;
